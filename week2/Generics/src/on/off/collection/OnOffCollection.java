f219b46f1fddee4abcf4eaac260c0519ebd249d8
tonynho <tonynho010@gmail.com>
2015-04-02 16:31:54 +0300

    implement OnOffCollection

--- week2/Generics/src/on/off/collection/OnOffCollection.java
+++ week2/Generics/src/on/off/collection/OnOffCollection.java
@@ -8,10 +8,14 @@ public class OnOffCollection<T> extends ArrayList<T> {
      */
     private static final long serialVersionUID = 3188050246807287931L;
 
+    /**
+     * @return false if elements was removed, else true
+     */
     @Override
     public boolean add(T e) {
         if (this.contains(e)) {
             this.remove(e);
+            return false;
         }
         return super.add(e);
     }
@@ -19,6 +23,21 @@ public class OnOffCollection<T> extends ArrayList<T> {
     @Override
     public void add(int index, T element) {
         // TODO Auto-generated method stub
-        super.add(index, element);
+        if (this.get(index).equals(element)) {
+            this.remove(index);
+        } else {
+            super.add(index, element);
+        }
+    }
+    
+    public static void main(String[] args) {
+        OnOffCollection<Integer> onoff = new OnOffCollection<Integer>();
+        onoff.add(1);
+        onoff.add(2);
+        onoff.add(2);
+        System.out.println(onoff.size());
+        onoff.add(2);
+        onoff.add(0, 1);
+        System.out.println(onoff.size());
     }
 }
