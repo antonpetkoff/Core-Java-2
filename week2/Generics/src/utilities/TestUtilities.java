6036c53261598e517538b4d8f1990c1241d3c098
tonynho <tonynho010@gmail.com>
2015-04-02 19:27:49 +0300

    implement and test problem 5 from Generics

--- week2/Generics/src/utilities/TestUtilities.java
+++ week2/Generics/src/utilities/TestUtilities.java
@@ -7,34 +7,33 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class TestUtilities {
-    
+
     private ArrayList<Integer> list = new ArrayList<Integer>();
-    
+
     @Before
     public void setUp() {
         for (int i = 1; i <= 7; i++) {
             list.add(i);
         }
     }
-    
+
     @Test
     public void testAreBracketsBalanced() {
         assertEquals(true, Utilities.areBracketsBalanced("((()())())"));
         assertEquals(false, Utilities.areBracketsBalanced("))())"));
         assertEquals(false, Utilities.areBracketsBalanced("((())))))())"));
     }
-    
+
     @Test
     public void testReverse() {
         Utilities.reverse(list);
-        
+
         for (int i = 1; i <= 7; i++) {
             assertEquals(list.get(i - 1), Integer.valueOf(list.size() + 1 - i));
         }
     }
-    
+
     @Test
     public void testRotateRight() {
         Utilities.rotate(list, 3);
@@ -45,7 +44,7 @@ public class TestUtilities {
         assertEquals(list.get(4), Integer.valueOf(2));
         assertEquals(list.size(), 7);
     }
-    
+
     @Test
     public void testRotateLeft() {
         Utilities.rotate(list, -9);
@@ -54,5 +53,19 @@ public class TestUtilities {
         assertEquals(list.get(6), Integer.valueOf(2));
         assertEquals(list.size(), 7);
     }
-    
+
+    @Test
+    public void testFindFirstUnique() {
+        for (int i = 1; i < 10; i++) {
+            list.add(i);
+        }
+
+        list.set(5, 5);
+        list.set(6, 4);
+        list.set(7, 3);
+        list.set(8, 1);
+
+        assertEquals(Integer.valueOf(2), Utilities.findFirstUnique(list));
+    }
+
 }
--- week2/Generics/src/utilities/Utilities.java
+++ week2/Generics/src/utilities/Utilities.java
@@ -4,11 +4,13 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Stack;
 
+import on.off.collection.OnOffCollection;
+
 public class Utilities {
-    
-    public static boolean areBracketsBalanced(String str) {        
+
+    public static boolean areBracketsBalanced(String str) {
         Stack<Character> stack = new Stack<Character>();
-        
+
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '(') {
                 stack.push(str.charAt(i));
@@ -19,22 +21,22 @@ public class Utilities {
                 stack.pop();
             }
         }
-        
+
         return stack.size() == 0;
     }
-    
+
     @SuppressWarnings("unchecked")
     public static <T> void reverse(Collection<T> collection) {
         Object[] arr = collection.toArray();
-        
+
         for (int i = 0; i < arr.length / 2; i++) {
             Object temp = arr[i];
             arr[i] = arr[arr.length - i - 1];
-            arr[arr.length - i - 1]  = temp;
+            arr[arr.length - i - 1] = temp;
         }
-        
+
         collection.clear();
-        
+
         for (int i = 0; i < arr.length; i++) {
             collection.add((T) arr[i]);
         }
@@ -44,13 +46,12 @@ public class Utilities {
         if (rotateStep == 0) {
             return;
         }
-        
+
         LinkedList<T> deque = new LinkedList<T>();
         deque.addAll(c);
-        
+
         rotateStep %= deque.size();
-        
-        
+
         if (rotateStep > 0) {
             while (rotateStep > 0) {
                 T temp = deque.removeLast();
@@ -64,12 +65,22 @@ public class Utilities {
                 ++rotateStep;
             }
         }
-        
+
         c.clear();
-        
+
         for (T elem : deque) {
             c.add(elem);
         }
     }
-    
+
+    public static <T> T findFirstUnique(Collection<T> c) {
+        OnOffCollection<T> temp = new OnOffCollection<T>();
+
+        for (T elem : c) {
+            temp.add(elem);
+        }
+
+        return temp.size() > 0 ? temp.get(0) : null;
+    }
+
 }
