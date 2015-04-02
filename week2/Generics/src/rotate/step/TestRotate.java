52a0bb71dc39e469f49b6fc96648da67cf87d6c2
tonynho <tonynho010@gmail.com>
2015-04-02 18:56:22 +0300

    rotate collection with tests

---
+++ week2/Generics/src/rotate/step/TestRotate.java
@@ -0,0 +1,42 @@
+package rotate.step;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestRotate {
+    
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    
+    @Before
+    public void setUp() {
+        for (int i = 1; i <= 7; i++) {
+            list.add(i);
+        }
+    }
+    
+    @Test
+    public void rotateRight() {
+        RotateCollection.rotate(list, 3);
+        assertEquals(list.get(0), Integer.valueOf(5));
+        assertEquals(list.get(1), Integer.valueOf(6));
+        assertEquals(list.get(2), Integer.valueOf(7));
+        assertEquals(list.get(3), Integer.valueOf(1));
+        assertEquals(list.get(4), Integer.valueOf(2));
+        assertEquals(list.size(), 7);
+    }
+    
+    @Test
+    public void rotateLeft() {
+        RotateCollection.rotate(list, -9);
+        assertEquals(list.get(4), Integer.valueOf(7));
+        assertEquals(list.get(5), Integer.valueOf(1));
+        assertEquals(list.get(6), Integer.valueOf(2));
+        assertEquals(list.size(), 7);
+    }
+    
+}
