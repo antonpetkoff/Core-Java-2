c4a894a23a4df57d0d7d8ab3067fffd177407ea1
tonynho <tonynho010@gmail.com>
2015-04-04 00:26:06 +0300

    test the sorted sequence

--- week2/Generics/src/statistics/TestSortedSequence.java
+++ week2/Generics/src/statistics/TestSortedSequence.java
@@ -5,37 +5,68 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSortedSequence {
 
-    @Test
-    public void testSortedSequenceAdd() {
-        SortedSequence seq = new SortedSequence();
-        
+    private SortedSequence seq;
+    private final static int INT_COUNT = 100;
+    
+    @Before
+    public void setUp() {
+        seq = new SortedSequence();
+        fillSequenceRandomly();
+    }
+    
+    private void fillSequenceRandomly() {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < INT_COUNT; i++) {
             list.add(i);
         }
 
         Collections.shuffle(list);
-        
-        System.out.println(list);
 
         for (Integer integer : list) {
             seq.add(integer);
         }
-        
-
+    }
+    
+    @Test
+    public void testSequenceSortedInvariant() {
         int i = 0;
         for (Integer integer : seq) {
-            System.out.println(integer);
             assertEquals(Integer.valueOf(i), integer);
             ++i;
         }
+    }
+    
+    @Test
+    public void testAddEqualNumbers() {
+        seq = new SortedSequence();
+        seq.add(6);
+        seq.add(5);
+        seq.add(7);
+        seq.add(6);
+        seq.add(6);
+        seq.add(7);
+        seq.add(5);
+        seq.add(7);
+        seq.add(5);
         
-        System.out.println(seq.getMedian());
-
+        int i = 0, expected = 4;
+        for (Integer integer : seq) {
+            if (i % 3 == 0) {
+                ++expected;
+            }
+            ++i;
+            assertEquals(Integer.valueOf(expected), integer);
+        }
+    }
+    
+    @Test
+    public void testGetMedian() {
+        assertEquals(Double.valueOf(49.5), Double.valueOf(seq.getMedian()));
     }
 
 }
