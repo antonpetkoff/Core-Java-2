735039a28e6c9a296d882176a8a549c47de48026
tonynho <tonynho010@gmail.com>
2015-04-05 14:28:17 +0300

    add tests for hash map without null keys

--- week2/Exceptions1/src/hashmap/nonull/TestHashMapNoNull.java
+++ week2/Exceptions1/src/hashmap/nonull/TestHashMapNoNull.java
@@ -1,12 +1,67 @@
 package hashmap.nonull;
 
+import java.util.HashMap;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TestHashMapNoNull {
 
+    private HashMapNoNull<Integer, Integer> map;
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    
     @Test
-    public void testPut() {
+    public void testPutNoNull() {
+        exception.expect(IllegalNullHashMapKey.class);
+        map = new HashMapNoNull<Integer, Integer>();    // doesn't accept null keys
+        map.put(null, 1);
+    }
+    
+    @Test
+    public void testPutWithNull() {
+        map = new HashMapNoNull<Integer, Integer>(true);
+        map.put(null, 2);
+    }
+    
+    @Test
+    public void testGetNoNull() {
+        exception.expect(IllegalNullHashMapKey.class);
+        map = new HashMapNoNull<Integer, Integer>(false);
+        map.get(null);
+    }
+    
+    @Test
+    public void testGetWithNull() {
+        map = new HashMapNoNull<Integer, Integer>(true);
+        map.get(null);
+    }
+    
+    @Test
+    public void testPutAllNoNull() {
+        map = new HashMapNoNull<Integer, Integer>(false);
+        HashMap<Integer, Integer> elements = new HashMap<Integer, Integer>();
+        elements.put(1, 2);
+        elements.put(2, 55);
+        elements.put(6, 124);
+        elements.put(null, 7);
         
+        exception.expect(IllegalNullHashMapKey.class);
+        map.putAll(elements);
+    }
+    
+    @Test
+    public void testPutAllWithNull() {
+        map = new HashMapNoNull<Integer, Integer>(true);
+        HashMap<Integer, Integer> elements = new HashMap<Integer, Integer>();
+        elements.put(1, 2);
+        elements.put(2, 55);
+        elements.put(6, 124);
+        elements.put(null, 7);
+        
+        map.putAll(elements);
     }
     
 }
