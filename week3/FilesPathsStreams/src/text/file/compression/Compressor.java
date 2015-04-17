d31d8c36c879e9ba0f8fe652a57e6fb7a0b82405
tonynho <tonynho010@gmail.com>
2015-04-17 21:59:54 +0300

    start text compressor

---
+++ week3/FilesPathsStreams/src/text/file/compression/Compressor.java
@@ -0,0 +1,18 @@
+package text.file.compression;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class Compressor {
+
+    private HashMap<Integer, String> map;
+    
+    public Compressor() {
+        map = new HashMap<Integer, String>();
+    }
+    
+    public void compress(Path filePath) {
+        // TODO
+    }
+    
+}
