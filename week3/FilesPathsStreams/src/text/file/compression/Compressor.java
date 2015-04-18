d19d882ed95df0d3f4cc1bb5ad623f017af1a0ee
tonynho <tonynho010@gmail.com>
2015-04-18 09:52:01 +0300

    implementing compress

--- week3/FilesPathsStreams/src/text/file/compression/Compressor.java
+++ week3/FilesPathsStreams/src/text/file/compression/Compressor.java
@@ -1,18 +1,62 @@
 package text.file.compression;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
+
+import file.utils.FileUtils;
 
 public class Compressor {
 
-    private HashMap<Integer, String> map;
+    private HashMap<String, Integer> map;
     
     public Compressor() {
-        map = new HashMap<Integer, String>();
+        map = new HashMap<String, Integer>();
     }
     
     public void compress(Path filePath) {
-        // TODO
+        String text = null;
+        try {
+            text = FileUtils.readFrom(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        int counter = 0;
+        StringBuilder compressed = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        
+        for (int i = 0; i < text.length(); ++i) {
+            if (text.charAt(i) == ' ') {
+                compressed.append(text.charAt(i));
+            } else {
+                while (i < text.length() && text.charAt(i) != ' ') {
+                    temp.append(text.charAt(i));
+                    ++i;
+                }
+                
+                if (!map.containsKey(temp)) {
+                    map.put(temp.toString(), counter);
+                    ++counter;
+                }
+                
+                compressed.append("~" + map.get(temp));
+                temp = new StringBuilder();
+            }
+        }
+        
+        String fileName = filePath.getFileName().toString().replaceFirst("\\..*", "");
+        String writePath = filePath.getParent().toString() + fileName + ".compr";
+        
+        try {
+            FileUtils.writeTo(compressed.toString(), Paths.get(writePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
