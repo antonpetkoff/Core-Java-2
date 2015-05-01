361241328968ec96fa1d7fb4d2cb2116db1f1f78
tonynho <tonynho010@gmail.com>
2015-05-01 22:20:21 +0300

    filter good links

--- week4/REST/src/web/crawler/WebCrawler.java
+++ week4/REST/src/web/crawler/WebCrawler.java
@@ -18,10 +18,12 @@ import org.apache.http.impl.client.HttpClients;
 public class WebCrawler {
 
     private CloseableHttpClient httpclient;
+    private String domain;
     private String rootURL;
     
-    public WebCrawler(String rootURL) {
+    public WebCrawler(String domain, String rootURL) {
         httpclient = HttpClients.createDefault();
+        this.domain = domain;
         this.rootURL = rootURL;
     }
     
@@ -48,6 +50,40 @@ public class WebCrawler {
         return content.toString();
     }
     
+    private boolean isSameDomain(String url) {
+        if (url.startsWith("http://")) {
+            if (url.startsWith("www.", 7)) {
+                return url.substring(11).startsWith(domain);
+            } else {
+                return url.substring(7).startsWith(domain);
+            }
+        } else if (url.startsWith("https://")) {
+            if (url.startsWith("www.", 8)) {
+                return url.substring(12).startsWith(domain);
+            } else {
+                return url.substring(8).startsWith(domain);
+            }
+        }
+        return false;
+    }
+    
+    public List<String> filterLinks(String rootURL, List<String> links) {
+        List<String> filtered = new ArrayList<String>();
+
+        for (String link : links) {
+            if (link.startsWith("/")) {
+                filtered.add(rootURL + link.substring(1));
+            }
+            else if (link.startsWith("#") || link.isEmpty() || !isSameDomain(link)) {
+                continue;
+            } else {
+                filtered.add(link);
+            }
+        }
+        
+        return filtered;
+    }
+    
     public List<String> getAllLinks(String content) {
         List<String> resultList = new ArrayList<>();
         String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
@@ -63,11 +99,17 @@ public class WebCrawler {
         return rootURL;
     }
     
+    public String getDomain() {
+        return domain;
+    }
+    
     public static void main(String[] args) throws ClientProtocolException, IOException {
-        WebCrawler wc = new WebCrawler("http://google.com");
+        WebCrawler wc = new WebCrawler("hackbulgaria.com", "https://hackbulgaria.com/");
         String content = wc.getContent("https://hackbulgaria.com/");
-        System.out.println(content);
-        System.out.println(wc.getAllLinks(content));
+        System.out.println(content + '\n');
+        List<String> links = wc.getAllLinks(content);
+        System.out.println(links);
+        System.out.println(wc.filterLinks(wc.getRootURL(), links));
     }
 
     
