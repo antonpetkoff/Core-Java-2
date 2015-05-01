c40a23667c07a936ae4041b154c3b8cf361a8d6f
tonynho <tonynho010@gmail.com>
2015-05-01 21:33:59 +0300

    get content in webcrawler

--- week4/REST/src/web/crawler/WebCrawler.java
+++ week4/REST/src/web/crawler/WebCrawler.java
@@ -1,20 +1,55 @@
 package web.crawler;
 
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class WebCrawler {
 
-    public static String getContent(URL url) {
-        // TODO
-        return null;
+    private CloseableHttpClient httpclient;
+    private String rootURL;
+    
+    public WebCrawler(String rootURL) {
+        httpclient = HttpClients.createDefault();
+        this.rootURL = rootURL;
     }
     
-    private static List<String> getAllLinks(String content) {
-        ArrayList<String> resultList = new ArrayList<>();
+    public String getContent(String url) throws ClientProtocolException, IOException {
+        HttpGet httpget = new HttpGet(url);
+        CloseableHttpResponse response = null;
+        StringBuilder content = new StringBuilder();
+        
+        try {
+            response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            String line = null;
+            
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));) {
+                while ((line = br.readLine()) != null) {
+                    content.append(line);
+                }
+            }
+            
+        } finally {
+            response.close();
+        }
+        
+        return content.toString();
+    }
+    
+    public List<String> getAllLinks(String content) {
+        List<String> resultList = new ArrayList<>();
         String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(content);
@@ -23,5 +58,17 @@ public class WebCrawler {
         }
         return resultList;
     }
+
+    public String getRootURL() {
+        return rootURL;
+    }
+    
+    public static void main(String[] args) throws ClientProtocolException, IOException {
+        WebCrawler wc = new WebCrawler("http://google.com");
+        String content = wc.getContent("https://hackbulgaria.com/");
+        System.out.println(content);
+        System.out.println(wc.getAllLinks(content));
+    }
+
     
 }
