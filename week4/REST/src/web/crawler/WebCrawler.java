89d4ee5dd2adee85a603b97a34ce85a3db2c0265
tonynho <tonynho010@gmail.com>
2015-05-01 23:00:08 +0300

    fully functional web crawler

--- week4/REST/src/web/crawler/WebCrawler.java
+++ week4/REST/src/web/crawler/WebCrawler.java
@@ -3,8 +3,15 @@ package web.crawler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Queue;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,19 +22,27 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+
 public class WebCrawler {
 
     private CloseableHttpClient httpclient;
     private String domain;
     private String rootURL;
     
-    public WebCrawler(String domain, String rootURL) {
+    /**
+     * 
+     * @param domain
+     * @param rootURL   always ends with /
+     * @throws MalformedURLException
+     * @throws URISyntaxException
+     */
+    public WebCrawler(String domain, String rootURL) throws MalformedURLException, URISyntaxException {
         httpclient = HttpClients.createDefault();
         this.domain = domain;
-        this.rootURL = rootURL;
+        this.rootURL = new URL(rootURL + "/").toURI().normalize().toString();
     }
     
-    public String getContent(String url) throws ClientProtocolException, IOException {
+    private String getContent(String url) throws ClientProtocolException, IOException {
         HttpGet httpget = new HttpGet(url);
         CloseableHttpResponse response = null;
         StringBuilder content = new StringBuilder();
@@ -67,16 +82,17 @@ public class WebCrawler {
         return false;
     }
     
-    public List<String> filterLinks(String rootURL, List<String> links) {
+    private List<String> filterLinks(String rootURL, List<String> links) {
         List<String> filtered = new ArrayList<String>();
 
         for (String link : links) {
-            if (link.startsWith("/")) {
-                filtered.add(rootURL + link.substring(1));
-            }
-            else if (link.startsWith("#") || link.isEmpty() || !isSameDomain(link)) {
+            if (link.startsWith(".") || link.startsWith("#") || link.isEmpty()) {
                 continue;
-            } else {
+            } else if (link.startsWith("/")) {
+                filtered.add(rootURL + link.substring(1));
+            } else if (!link.startsWith("/") && link.endsWith(".html")) {
+                filtered.add(rootURL + link);
+            } else if (isSameDomain(link)) {
                 filtered.add(link);
             }
         }
@@ -84,7 +100,7 @@ public class WebCrawler {
         return filtered;
     }
     
-    public List<String> getAllLinks(String content) {
+    private List<String> getAllLinks(String content) {
         List<String> resultList = new ArrayList<>();
         String regex = "<a.*?href=\"((?!javascript).*?)\".*?>";
         Pattern pattern = Pattern.compile(regex);
@@ -93,6 +109,44 @@ public class WebCrawler {
             resultList.add(matcher.group(1));
         }
         return resultList;
+    }
+    
+    /**
+     * 
+     * @param needle    String to search for
+     * @return  a list of all links which contain needle
+     */
+    public List<String> find(String needle) {
+        Set<String> visited = new TreeSet<String>();
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(rootURL);
+        List<String> result = new ArrayList<String>();
+        
+        String content = null, tempURL = null;
+        List<String> tempList = null;
+        
+        while(!queue.isEmpty()) {
+            tempURL = queue.remove();
+            try {
+                content = getContent(tempURL);
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (content.contains(needle)) {
+                result.add(tempURL);
+            }
+            
+            tempList = filterLinks(tempURL, getAllLinks(content));
+            for (String link : tempList) {
+                if (!visited.contains(link)) {
+                    queue.add(link);
+                }
+            }
+        }
+        
+        return result;
     }
 
     public String getRootURL() {
@@ -103,14 +157,9 @@ public class WebCrawler {
         return domain;
     }
     
-    public static void main(String[] args) throws ClientProtocolException, IOException {
-        WebCrawler wc = new WebCrawler("hackbulgaria.com", "https://hackbulgaria.com/");
-        String content = wc.getContent("https://hackbulgaria.com/");
-        System.out.println(content + '\n');
-        List<String> links = wc.getAllLinks(content);
-        System.out.println(links);
-        System.out.println(wc.filterLinks(wc.getRootURL(), links));
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException {
+        WebCrawler wc = new WebCrawler("ebusiness.free.bg", "http://ebusiness.free.bg");
+        System.out.println(wc.find("код"));
     }
 
-    
 }
