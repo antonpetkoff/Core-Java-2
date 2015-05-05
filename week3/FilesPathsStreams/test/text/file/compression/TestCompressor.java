6288e627446c3581286d608226b63146aafa70c9
tonynho <tonynho010@gmail.com>
2015-05-05 23:54:36 +0300

    test Compressor

--- week3/FilesPathsStreams/test/text/file/compression/TestCompressor.java
+++ week3/FilesPathsStreams/test/text/file/compression/TestCompressor.java
@@ -2,11 +2,18 @@ package text.file.compression;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import file.utils.FileUtils;
 
 public class TestCompressor {
 
@@ -25,6 +32,22 @@ public class TestCompressor {
         ArrayList<String> decoded = (ArrayList<String>) Compressor.deserializeFromString(encoded);
         
         assertTrue(list.equals(decoded));
+    }
+    
+    @Test
+    public void testCompression() throws FileNotFoundException, IOException {
+        String filePath = "res/" + UUID.randomUUID().toString();
+        String testText = "buffalo buffalo buffalo rides a buffalo buffalo buffalo.";
+        FileUtils.writeTo(testText, Paths.get(filePath + ".txt"));
+        Compressor.compress(Paths.get(filePath + ".txt"));
+        Compressor.decompress(Paths.get(filePath + ".compr"));
+        String decompressed = FileUtils.readFrom(Paths.get(filePath + ".decompr"));
+        
+        Files.delete(Paths.get(filePath + ".txt"));
+        Files.delete(Paths.get(filePath + ".compr"));
+        Files.delete(Paths.get(filePath + ".decompr"));
+        
+        assertEquals(testText, decompressed);
     }
     
 }
