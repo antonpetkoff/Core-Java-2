95db4d4dd8b5e4ed3bdceb1c7ca1bfbca3ba523a
tonynho <tonynho010@gmail.com>
2015-05-13 12:34:27 +0300

    finilize nearest points

--- week6/Multithreading/src/minimal/distance/NearestPoint.java
+++ week6/Multithreading/src/minimal/distance/NearestPoint.java
@@ -35,18 +35,18 @@ public class NearestPoint {
         }
     }
 
-    public static Map<Point, Point> getNearestPoints(final List<Point> generatedPoints) throws IllegalArgumentException {
+    public static Map<Point, Point> getNearestPoints(final List<Point> generatedPoints, int threadsCount) throws IllegalArgumentException {
         final Map<Point, Point> outMap = new ConcurrentHashMap<Point, Point>();
 
-        Thread[] threads = new Thread[THREADS_COUNT];
-        final int slice = generatedPoints.size() / THREADS_COUNT;
+        Thread[] threads = new Thread[threadsCount];
+        final int slice = generatedPoints.size() / threadsCount;
 
-        for (int i = 0; i < THREADS_COUNT; ++i) {
+        for (int i = 0; i < threadsCount; ++i) {
             threads[i] = new Thread(new Calculation(generatedPoints, 0 + i * slice, (i + 1) * slice, outMap));
             threads[i].start();
         }
         
-        for (int i = 0; i < THREADS_COUNT; ++i) {
+        for (int i = 0; i < threadsCount; ++i) {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
@@ -58,16 +58,20 @@ public class NearestPoint {
         return outMap;
     }
 
+    // THREADS_COUNT must divide POINTS_COUNT
     public static final int THREADS_COUNT = 100;
     
-    public static final Object lock = new Object();
-
+    public static final int DIMENSIONS_COUNT = 3;
+    public static final int POINTS_COUNT = 100_000;
+    public static final int RANGE_MIN = 0;
+    public static final int RANGE_MAX = 10_000;
+    
     public static void main(String[] args) throws IllegalArgumentException {
-        List<Point> generatedPoints = Point.generatePoints(3, 100_000, 0, 10_000);
+        List<Point> generatedPoints = Point.generatePoints(DIMENSIONS_COUNT, POINTS_COUNT, RANGE_MIN, RANGE_MAX);
 
         long start = System.currentTimeMillis();
 
-        Map<Point, Point> map = NearestPoint.getNearestPoints(generatedPoints);
+        Map<Point, Point> map = NearestPoint.getNearestPoints(generatedPoints, THREADS_COUNT);
 
         System.out.println("Duration: " + (System.currentTimeMillis() - start) / 1000.0 + " seconds");
     }
